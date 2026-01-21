ea31bfce2c417b146e5b9e01345cfbb4672f9742
MaximoRossa <maximorossam@gmail.com>
2026-01-21 16:06:10 -0300

    First commit

---
+++ src/main/java/curso/geekipedia/CursoDeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package curso.geekipedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/curso/geekipedia/CursoDeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.geekipedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
